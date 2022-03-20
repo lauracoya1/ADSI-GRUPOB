4d5ba8b3617d86f6e9d65b3b81ecac57aba48f50
Carlos Manuel Varela de fátima <carlos.varela1@udc.es>
2022-03-20 12:01:10 +0100

    DNI users

--- src/main/resources/sql/F02_insert_users.sql
+++ src/main/resources/sql/F02_insert_users.sql
@@ -1,5 +1,5 @@
-INSERT INTO Users (name, user_role, greeting, password)
+INSERT INTO Users (name, user_role, greeting, password, dni)
 VALUES
-	('admin', 1, 'Hola admin', 'adminpass'),
-	('agent', 2, 'Hola agent', 'agentpass'),
-	('client', 3, 'Hola client', 'clientpass')
+	('admin', 1, 'Hola admin', 'adminpass', '1'),
+	('agent', 2, 'Hola agent', 'agentpass','2'),
+	('client', 3, 'Hola client', 'clientpass','3')
